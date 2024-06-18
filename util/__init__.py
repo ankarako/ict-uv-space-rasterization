dc6b148648217be950d16e442e5f46d198cc2f38
perukas <ankarakottas@gmail.com>
2024-06-18 23:37:11 +0300

    + rasterize_triangle

--- util/__init__.py
+++ util/__init__.py
@@ -1,3 +1,2 @@
 import util.conf as conf
 import util.io as io
-import util.mesh_props as mesh_props
--- util/io.py
+++ util/io.py
@@ -1,9 +1,74 @@
 from typing import Tuple
 import os
-import tinyobjloader as tinyobj
 import torch
+import numpy as np
 
 
+def parse_obj(filepath: str) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+    """
+    Parse an obj file
+    """
+    def parse_poly(line: str):
+        """
+        Parse a polygon, also triangulate
+        """
+        poly_info = line.split(' ')[1:]
+        vp_idx = []
+        vt_idx = []
+        if len(poly_info) == 3:
+            for info in poly_info:
+                vp = info.split('/')[0]
+                # obj counts from 1
+                vp_idx += [int(vp) - 1]
+                vt = info.split('/')[1]
+                # obj counts from 1
+                vt_idx += [int(vt) - 1]
+            vp_idx = [torch.tensor(vp_idx)]
+            vt_idx = [torch.tensor(vt_idx)]
+        elif len(poly_info) == 4:
+            v1, v2, v3, v4 = poly_info
+            # obj counts from 1
+            vp_idx += [torch.tensor([int(v1.split('/')[0]) - 1, int(v2.split('/')[0]) - 1, int(v3.split('/')[0]) - 1])]
+            vp_idx += [torch.tensor([int(v3.split('/')[0]) - 1, int(v4.split('/')[0]) - 1, int(v1.split('/')[0]) - 1])]
+            vt_idx += [torch.tensor([int(v1.split('/')[1]) - 1, int(v2.split('/')[1]) - 1, int(v3.split('/')[1]) - 1])]
+            vt_idx += [torch.tensor([int(v3.split('/')[1]) - 1, int(v4.split('/')[1]) - 1, int(v1.split('/')[1]) - 1])]
+        return vp_idx, vt_idx
+
+    if not os.path.exists(filepath):
+        print(f'Error: The specified filepath is invalid: {filepath}')
+    
+    with open(filepath, 'r') as infd:
+        obj_lines = infd.readlines()
+    v_pos = []
+    v_uvs = []
+    t_pos_idx = []
+    t_uvs_idx = []
+    for obj_line in obj_lines:
+        obj_line = obj_line.replace('\n', '')
+        if obj_line.startswith('v') and not (obj_line.startswith('vt') or obj_line.startswith('vn')):
+            vpos = obj_line.split(' ')[1:]
+            vpos = [float(v) for v in vpos]
+            assert len(vpos) == 3
+            v_pos += [torch.tensor([vpos])]
+        elif obj_line.startswith('vt'):
+            vt = obj_line.split(' ')[1:]
+            vt = [float(v) for v in vt]
+            v_uvs += [torch.tensor([vt])]
+        elif obj_line.startswith('f'):
+            vp_idx, vt_idx = parse_poly(obj_line)
+            for vp_i in vp_idx:
+                t_pos_idx += [vp_i]
+            for vt_i in vt_idx:
+                t_uvs_idx += [vt_i]
+        else:
+            continue
+    v_pos = torch.vstack(v_pos).float().cuda()
+    v_uvs = torch.vstack(v_uvs).float().cuda()
+    t_pos_idx = torch.vstack(t_pos_idx).long().cuda()
+    t_uvs_idx = torch.vstack(t_uvs_idx).long().cuda()
+    return v_pos, v_uvs, t_pos_idx, t_uvs_idx
+        
+    
 def load_generic_model(dir: str) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
     """
     Load ict facekit's generic model
@@ -11,27 +76,8 @@ def load_generic_model(dir: str) -> Tuple[torch.Tensor, torch.Tensor, torch.Tens
     :param dir The directory with ICTFaceKit's data.
     :return
     """
-    reader = tinyobj.ObjReader()
-    filepath = os.path.join(dir, 'generic_neutral_mesh.obj')
-    if not os.path.exists(filepath):
-        print("The specified ICT directory does not contain generic_neutral_mesh.obj")
-        return None
-    obj = reader.ParseFromFile(filepath)
-    attrib = reader.GetAttrib()
-    materials = reader.GetMaterials()
-    shapes = reader.GetShapes()
-
-    v_pos = torch.tensor(list(attrib.vertices), dtype=torch.float32).reshape(-1, 3)
-    v_uvs = torch.tensor(list(attrib.texcoords), dtype=torch.float32).reshape(-1, 3)
-    shape = shapes[-1] # we probably know that we expect a single shape
-
-    t_pos_idx = []
-    t_uvs_idx = []
-    for idx_entry in shape.mesh.indices:
-        t_pos_idx += [idx_entry.vertex_index]
-        t_uvs_idx += [idx_entry.texcoord_index]
-    t_pos_idx = torch.tensor(t_pos_idx, dtype=torch.long).reshape(-1, 4)
-    t_uvs_idx = torch.tensor(t_uvs_idx, dtype=torch.long).reshape(-1, 4)
+    filepath = os.path.join(dir, "generic_neutral_mesh.obj")
+    v_pos, v_uvs, t_pos_idx, t_uvs_idx = parse_obj(filepath)
     return v_pos, v_uvs, t_pos_idx, t_uvs_idx
 
 
